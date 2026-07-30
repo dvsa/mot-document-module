25a1c34b4ad4b46ce089c3176bcab2891b47104a
aleksandra.mordzon <aleksandra.mordzon@defra.onmicrosoft.com>
2026-07-30 16:34:55 +0200

    feat: added tests for enhancedlambdafactory

---
+++ test/DvsaReport/Service/Factory/EnhancedLambdaHttpClientServiceFactoryTest.php
@@ -0,0 +1,162 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DvsaReportModuleTest\DvsaReport\Service\Factory;
+
+use DvsaLogger\Logger\MotLogger;
+use DvsaReport\Service\Factory\EnhancedLambdaHttpClientServiceFactory;
+use DvsaReport\Service\HttpClient\EnhancedLambdaHttpClientService;
+use Laminas\ServiceManager\Exception\ServiceNotFoundException;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Container\ContainerInterface;
+use RuntimeException;
+
+final class EnhancedLambdaHttpClientServiceFactoryTest extends TestCase
+{
+    private EnhancedLambdaHttpClientServiceFactory $factory;
+
+    private ContainerInterface&MockObject $container;
+
+    private MotLogger&MockObject $logger;
+
+    private array $validConfig;
+
+    #[\Override]
+    public function setUp(): void
+    {
+        $this->factory = new EnhancedLambdaHttpClientServiceFactory();
+
+        $this->logger = $this->getMockBuilder(MotLogger::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->container = $this->getMockBuilder(ContainerInterface::class)
+            ->getMock();
+
+        $this->validConfig = [
+            'certificate_generation' => [
+                'headers'                  => ['Content-Type' => 'application/json'],
+                'uri'                      => 'https://example.com/api',
+                'x-api-key'               => 'test-api-key',
+                'max_request_attempt_count' => 3,
+                'request_attempt_delay'    => 100,
+                'adapter'                  => \Laminas\Http\Client\Adapter\Curl::class,
+            ],
+        ];
+    }
+
+    public function testInvokeReturnsEnhancedLambdaHttpClientService(): void
+    {
+        $this->container->method('get')
+            ->willReturnMap([
+                ['Config', $this->validConfig],
+                ['Application\Logger', $this->logger],
+            ]);
+
+        $result = ($this->factory)($this->container, EnhancedLambdaHttpClientService::class);
+
+        $this->assertInstanceOf(EnhancedLambdaHttpClientService::class, $result);
+    }
+
+    public function testInvokeWithClientOptionsCallsSetOptions(): void
+    {
+        $configWithOptions = $this->validConfig;
+        $configWithOptions['certificate_generation']['client_options'] = ['timeout' => 30];
+
+        $this->container->method('get')
+            ->willReturnMap([
+                ['Config', $configWithOptions],
+                ['Application\Logger', $this->logger],
+            ]);
+
+        $result = ($this->factory)($this->container, EnhancedLambdaHttpClientService::class);
+
+        $this->assertInstanceOf(EnhancedLambdaHttpClientService::class, $result);
+    }
+
+    public function testInvokeThrowsExceptionWhenRootConfigKeyMissing(): void
+    {
+        $this->container->method('get')
+            ->with('Config')
+            ->willReturn([]);
+
+        $this->expectException(RuntimeException::class);
+
+        ($this->factory)($this->container, EnhancedLambdaHttpClientService::class);
+    }
+
+    /**
+     * @dataProvider provideRequiredConfigKeys
+     */
+    public function testInvokeThrowsExceptionWhenRequiredConfigKeyMissing(string $missingKey): void
+    {
+        $config = $this->validConfig;
+        unset($config['certificate_generation'][$missingKey]);
+
+        $this->container->method('get')
+            ->willReturnMap([
+                ['Config', $config],
+                ['Application\Logger', $this->logger],
+            ]);
+
+        $this->expectException(RuntimeException::class);
+
+        ($this->factory)($this->container, EnhancedLambdaHttpClientService::class);
+    }
+
+    public function provideRequiredConfigKeys(): array
+    {
+        return array_map(
+            fn(string $key) => [$key],
+            EnhancedLambdaHttpClientServiceFactory::REQUIRED_CONFIG_KEYS
+        );
+    }
+
+    public function testObtainLoggerThrowsExceptionWhenLoggerNotFoundInContainer(): void
+    {
+        $this->container->method('get')
+            ->willReturnCallback(function (string $id) {
+                if ($id === 'Config') {
+                    return $this->validConfig;
+                }
+                throw new ServiceNotFoundException('Service not found: ' . $id);
+            });
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('\DvsaLogger\Logger\MotLogger instance expected in ServiceLocator under Application\Logger');
+
+        ($this->factory)($this->container, EnhancedLambdaHttpClientService::class);
+    }
+
+    public function testObtainLoggerThrowsExceptionWhenLoggerIsNotMotLoggerInstance(): void
+    {
+        $invalidLogger = new \stdClass();
+
+        $this->container->method('get')
+            ->willReturnMap([
+                ['Config', $this->validConfig],
+                ['Application\Logger', $invalidLogger],
+            ]);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('\DvsaLogger\Logger\MotLogger instance expected in ServiceLocator under Application\Logger');
+
+        ($this->factory)($this->container, EnhancedLambdaHttpClientService::class);
+    }
+
+    public function testObtainLoggerThrowsExceptionWhenLoggerIsNull(): void
+    {
+        $this->container->method('get')
+            ->willReturnMap([
+                ['Config', $this->validConfig],
+                ['Application\Logger', null],
+            ]);
+
+        $this->expectException(RuntimeException::class);
+        $this->expectExceptionMessage('\DvsaLogger\Logger\MotLogger instance expected in ServiceLocator under Application\Logger');
+
+        ($this->factory)($this->container, EnhancedLambdaHttpClientService::class);
+    }
+}
